7de1cc3095332e878807456c5f9c645c86032747
Ahmad Bilal <ahmadbilaldev@gmail.com>
2021-02-06 19:54:48 +0500

    📦 NEW: App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home.js';
+import NewClass from './pages/NewClass.js';
+import Sessions from './pages/Sessions.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			{/* ROUTES */}
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/newclass" component={NewClass} />
+				<Route path="/sessions" component={Sessions} />
+			</Switch>
+		</>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
